3ea8f5722926ca3b7700702760099c1155285571
Jacob Hendrich <hendrijm@rose-hulman.edu>
2021-01-06 15:58:44 -0500

    Initial Commit

---
+++ MovieQuotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieQuotes
+//
+//  Created by Jacob Hendrich on 1/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
